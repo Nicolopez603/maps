526a0d2e1654999c2333b842090cfdbf7f5c790c
Nicolas Lopez <nicomlopez3@gmail.com>
2022-05-17 14:16:21 -0300

    src:CustomMap: Adding a custom map

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,4 @@
 /// <reference types="@types/google.maps" />
+import { CustomMap } from './CustomMap';
 
-import { User } from "./User";
-import { Company } from "./Company";
-
-const user = new User();
-const company = new Company();
-console.log(user, company);
-
-new google.maps.Map(document.getElementById("map"), {
-  center: { lat: 0, lng: 0 },
-  zoom: 1,
-});
+new CustomMap('map');
